f6fafc1c014168294f0d1014808e76f596d2770e
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-02-05 15:30:03 +0500

    in progress

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  res.render("login");
 });
+
+router.post("/");
 
 module.exports = router;
